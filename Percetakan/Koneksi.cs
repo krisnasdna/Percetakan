7e107e98ec4fcd22d043350c72c68f2e16c73478
krisnasdna <krisnasedana2403@gmail.com>
2024-01-23 16:22:56 +0800

    membuat class

--- Percetakan/Koneksi.cs
+++ Percetakan/Koneksi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace Percetakan
 {
     internal class Koneksi
     {
+        private static string connString;
+        public static SqlConnection sqlConn;
+
+        public static void buka()
+        {
+            connString = @"data source=(localdb)\percetakan; initial catalog=percetakan; integrated security = true";
+            sqlConn = new SqlConnection(connString);
+            if (sqlConn.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConn.Open();
+            }
+        }
+
+        public static void tutup()
+        {
+            if (sqlConn.State == System.Data.ConnectionState.Open)
+            {
+                sqlConn.Close();
+            }
+        }
     }
 }
